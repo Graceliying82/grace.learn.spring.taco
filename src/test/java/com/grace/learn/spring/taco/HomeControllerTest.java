481bebc61ae9c082ad9d8b371b6d640050fa268e
graceliying82 <gracejustingrace@gmail.com>
2024-01-25 07:07:37 -0500

    Initial Commit

---
+++ src/test/java/com/grace/learn/spring/taco/HomeControllerTest.java
@@ -0,0 +1,28 @@
+package com.grace.learn.spring.taco;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.hamcrest.Matchers.containsString;
+
+@WebMvcTest(HomeController.class)
+@Slf4j
+public class HomeControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testHomePage() throws Exception {
+        mockMvc.perform(get("/"))
+            .andExpect(status().isOk())
+            .andExpect(view().name("home"))
+            .andExpect(content().string(containsString("Welcome to...")));
+        log.info("Test testHomePage passed! ");
+
+    }
+}
